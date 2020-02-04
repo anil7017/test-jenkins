8bbc66a19aa863d993f8bfe2d76eb34a035d989a
anilkumar <anilkumarsahoo17@gmail.com>
2020-02-04 12:58:21 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/Testdemo.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testdemo {
+
+    @GetMapping("/test")
+    public String showMessage() {
+        System.out.println("inside hello controller......");
+        return "Hello Springboot";
+    }
+}
